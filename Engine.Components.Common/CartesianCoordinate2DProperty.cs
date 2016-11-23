211ad0e4f8955251b76abd11751262582c582cec
Jack Coldridge <jack@coldridge.com>
2016-11-23 00:46:49 +0000

    more component/system integration

---
+++ Engine.Components.Common/CartesianCoordinate2DProperty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine.Common;
+using Engine.Components.Property;
+using Engine.Core.Components;
+using Engine.Core.Entities;
+
+namespace Engine.Components.Common
+{
+	public class CartesianCoordinate2DProperty : Property<Vector>
+	{
+		public CartesianCoordinate2DProperty(IEntity entity, Vector value) 
+			: base(entity, value)
+		{
+		}
+	}
+}
