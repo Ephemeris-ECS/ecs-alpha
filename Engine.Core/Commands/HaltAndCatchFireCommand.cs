5a8fc083c3002cf5654622693fbd01591e93343f
Jared <jaredgglass@gmail.com>
2017-02-06 14:37:45 +0000

    halt and catch fire command

---
+++ Engine.Core/Commands/HaltAndCatchFireCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Engine.Commands
+{
+	public class HaltAndCatchFireCommand : ICommand
+	{
+	}
+
+	public class HaltAndCatchFireCommandHandler : CommandHandler<HaltAndCatchFireCommand>
+	{
+		protected override bool TryProcessCommand(HaltAndCatchFireCommand command)
+		{
+			throw new HaltAndCatchFireException();
+		}
+	}
+
+	public class HaltAndCatchFireException : Exception
+	{
+	}
+}
